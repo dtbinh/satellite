be11b2d4d56a39210d30cce25024f641af976349
rustygoh <goh@berlin-space-tech.com>
2019-01-23 18:40:22 +0100

    phhhew

--- bst_rwa_dm/pid_controller.m
+++ bst_rwa_dm/pid_controller.m
@@ -2,17 +2,20 @@ function out = pid_controller(out, in, pid, dt)
 
 persistent sum in_old;
 
-
 sum = sum + in*dt;
 
 if (pid.init==1)
     fprintf('pid init\n');
+    
     if(pid.ki ~= 0.0)
+        fprintf('pid integrator init\n');
         sum = out/pid.ki; 
     else
         sum = 0.0;
     end
+    
     in_old = in;
+    pid.init = 0;
 end
 
 
