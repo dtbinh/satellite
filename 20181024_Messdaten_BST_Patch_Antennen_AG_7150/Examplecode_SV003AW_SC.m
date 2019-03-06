734e2a6d8d1e5e286355870a1bc238ce5effe185
rustygoh <weixiong86@hotmail.com>
2019-03-07 00:45:56 +0100

    test

--- 20181024_Messdaten_BST_Patch_Antennen_AG_7150/Examplecode_SV003AW_SC.m
+++ 20181024_Messdaten_BST_Patch_Antennen_AG_7150/Examplecode_SV003AW_SC.m
@@ -15,7 +15,7 @@ p  = sqrt((p1 .* p1) + (p2 .* p2));
 
 % normiertes pattern
 plin = p ./ max(max(p(:,:))); % 0 bis 1
-plog = 20*log10(  max(plin, 10^(-dynamic/20))  )    + dynamic; % 0dB bis dynamic
+plog =-( 20*log10(  max(plin, 10^(-dynamic/20))  )    + dynamic); % 0dB bis dynamic
 
 % directivity
 PowerPatt = plin.* plin; % voltage pattern -> power pattern
@@ -23,9 +23,9 @@ Summe = 0;
 thetapts = length(pattern.dim(1).value);
 phipts = length(pattern.dim(2).value);
 
-for j = 1:phipts,
+for j = 1:phipts
   
-    for i = 1:thetapts,
+    for i = 1:thetapts
         Summe = Summe + (PowerPatt(j,i) * abs(sin(  (pattern.dim(1).value(i))/180*pi  ))) ; %Betrag vom Sinus notwendig wenn theta span -180??? bis + 180???
     end
 end
@@ -36,58 +36,52 @@ Dlin = 4*pi*max(max(PowerPatt)) / Prad;
 Dlog = 10*log10(Dlin);
 
 % Trafo in Matlab-surf's kartesisches Koordinatensystem
-% theta - azimuth
-% phi   - elevation
-[theta_int,phi_int] = meshgrid(pattern.measurement.dim(2).value, pattern.measurement.dim(1).value - 90 ); 
-[x,y,z]             = sph2cart(theta_int*pi/180, phi_int*pi/180, -plog);
+% theta - ele
+% phi   - azi
+[phi_int,theta_int] = meshgrid(pattern.measurement.dim(2).value, pattern.measurement.dim(1).value - 90 ); 
+% [x,y,z] = sph2cart(azimuth,elevation,r)
+
+[x_int,y_int,z_int] = sph2cart(phi_int*pi/180,theta_int*pi/180, plog); %[x,y,z] = sph2cart(azimuth,elevation,r)
+
+fig = figure;
+surf(phi_int,theta_int+90,-plog);
+set(fig,'Name','RAW');
+xlabel('phi [deg]');
+ylabel('theta [deg]');
 
 % ------------------------------------------------------------------------
 % Obtain Measurement Data
-[azi,elev,r] = cart2sph(x,y,z); 
+[azi_int,elev_int,r] = cart2sph(x_int,y_int,z_int); % THis function is not 100%
+
+% Refine Data
+for i = 1:121
+    for j = 1:61
+        azi(i,j) = azi_int(2,j)*180/pi;
+        elev(i,j) = elev_int(i,61)*180/pi;
+    end
+end
+
 
 % Set Plot
 di = 121;  % elev (121) -90 to 90  180 deg axis -> -z_axis -x direction
 dj = 61;   %  azi (61) -180 to 180 360 deg axis -> -x_axis x-y plane +z direction
 
-% Convert theta to Spherical Coordinate
-for j = 1:1:61        
-    for i=1:1:61          
-        theta(i,j) = 180 - round((i-1)*(180/60));
-    end
-end
-for j = 1:1:61        
-    for i=61:1:121          
-        theta(i,j) = round((i-1)*(180/60))-180;
-    end
-end
-
-% Convert phi to Spherical Coordinate
-for i = 1:1:61      
-    for j=1:1:61 
-        phi(i,j)   = -180 + round((j-1)*(180/60));
-    end
-end
-for i = 62:1:121      
-    for j=1:1:61 
-        phi(i,j)   = round((j-1)*(180/60));
-    end
-end
-
 % Mapping
-for i = 1:1:121   % elev 
+for i = 1:1:61   
     for j=1:1:61
-        
+        azi(i,j) = azi(i,j);
+        elev(i,j) = pi/2 - elev(i,j);
     end
 end
 
 
-for i = 1:1:61   % elev 
+for i = 1:1:121      % ele
     for j=1:1:61   % azi
-        fprintf('[%2d,%2d] azi: %11.6f ele: %11.6f phi(azi): %11.6f theta(ele): %11.6f phi_int: %11.6f theta_int: %11.6f\n',...
-            i,j,azi(i,j)*180/pi,elev(i,j)*180/pi, phi(i,j), theta(i,j), phi_int(i,j), theta_int(i,j)  );
+        fprintf('[%3d,%3d] azi: %8.2f ele: %8.2f\n',...
+            i,j,azi(i,j),elev(i,j));
         
         % Rotate Vector
-        vector_test(:,i,j)   = [x(i,j);y(i,j);z(i,j)];
+        vector_test(:,i,j)   = [x_int(i,j);y_int(i,j);z_int(i,j)];
         vector_sc_int(:,i,j) = dcm(1,pi/2)*dcm(3,-pi/2)*vector_test(:,i,j);       % BST
         vector_sc(:,i,j)     = dcm(3,-pi/2)*dcm(1,-pi/2)*vector_sc_int(:,i,j);    % STM
         
@@ -146,7 +140,7 @@ zcal = max(20*log10(pc(:,:)), Glog-dynamic);
 screensize = get(0,'ScreenSize');
 
 fig = figure;
-set(fig,'Position',[2*screensize(3)*0.5 screensize(4)*0.25  screensize(3)*0.5 screensize(4)*0.5]);
+set(fig,'Position',[screensize(3)*0.5 screensize(4)*0.25  screensize(3)*0.5 screensize(4)*0.5]);
 set(fig,'Name',upper(type));
 
 h3 = surf(xx,yy,zz); hold on;
@@ -158,7 +152,7 @@ cameratoolbar('SetCoordSys','none')
 set(gca,'CameraViewAngle',8); % Set Zoom of Graph
 set(get(colorbar,'title'),'String', 'gain [dBi]', 'FontSize', 18, 'fontname', 'arial'); 
 caxis manual; 
-% set(h3,'edgecolor','none'); 
+set(h3,'edgecolor','none'); 
 % shading interp;
 
 
@@ -184,6 +178,14 @@ usedfreq = pattern.dim(3).value(freq);
 %text('Dmax = ',Dlog);
 
 % rotate3d on; 
+
+% [X,Y] = meshgrid(0.5:0.5:5,1:20);
+% % X - 20x10
+% % Y - 20x10
+% % Z - 20x10
+% Z = sin(X) + cos(Y);
+% figure
+% surf(X,Y,Z)
 return
 %% ---------------------------------------------------------------------------
 % 2D Plot
@@ -193,11 +195,11 @@ fig = figure;
 % z_data = 20*log10(pc(:,:)');
 
 % Set plot
-x_data = theta(:,1);  % should be 0 to 180
-y_data = phi(2,:);   % should be -180 to 180;
-z_data = mag(:,:)';
+x_data = azi*180/pi;  % 
+y_data = elev*180/pi;    % 
+z_data = mag;
 
-set(fig,'Position',[screensize(3)*0.5 screensize(4)*0.5  screensize(3)*0.5 screensize(4)*0.5]);
+% set(fig,'Position',[screensize(3)*0.5 screensize(4)*0.5  screensize(3)*0.5 screensize(4)*0.5]);
 h4 = surf(x_data,y_data, z_data, 'EdgeColor','none');
 xlabel('Elevation: theta 0 to 180');
 ylabel('Azimuth:   phi -180 to 180');
