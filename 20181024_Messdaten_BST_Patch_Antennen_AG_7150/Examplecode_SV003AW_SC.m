0b5fbbc955f5c7a138be56ec3e2eb91d31155fa9
rustygoh <goh@berlin-space-tech.com>
2019-03-07 18:17:57 +0100

    ah

--- 20181024_Messdaten_BST_Patch_Antennen_AG_7150/Examplecode_SV003AW_SC.m
+++ 20181024_Messdaten_BST_Patch_Antennen_AG_7150/Examplecode_SV003AW_SC.m
@@ -5,15 +5,15 @@ clc;
 % Load Data
 load('BST_2_Patchantennen.mat');
 
-dynamic = 30; % dB
-freq    = 1;  % Vektorindex
-
 % uncal pattern combined / total
+freq    = 1;  % Vektorindex
 p1 = abs(pattern.value.uncal(:,:,freq,1));
 p2 = abs(pattern.value.uncal(:,:,freq,2));
 p  = sqrt((p1 .* p1) + (p2 .* p2));
 
+
 % normiertes pattern
+dynamic = 30; % dB
 plin = p ./ max(max(p(:,:))); % 0 bis 1
 plog =-( 20*log10(  max(plin, 10^(-dynamic/20))  )    + dynamic); % 0dB bis dynamic
 
@@ -34,59 +34,40 @@ umax=max(max(abs(plin)));
 
 Dlin = 4*pi*max(max(PowerPatt)) / Prad;
 Dlog = 10*log10(Dlin);
-
-% Trafo in Matlab-surf's kartesisches Koordinatensystem
-% theta - ele
-% phi   - azi
+% ------------------------------------------------------------------------
+% Obtain the phi and theta mesh grid
 [phi_int,theta_int] = meshgrid(pattern.measurement.dim(2).value, pattern.measurement.dim(1).value - 90 ); 
 % [x,y,z] = sph2cart(azimuth,elevation,r)
 
-[x_int,y_int,z_int] = sph2cart(phi_int*pi/180,theta_int*pi/180, plog); %[x,y,z] = sph2cart(azimuth,elevation,r)
 
-fig = figure;
-h3 = surf(phi_int,theta_int+90,-plog);
-set(get(colorbar,'title'),'String', 'gain [dBi]', 'FontSize', 18, 'fontname', 'arial');
-caxis([-20 30])
-set(h3,'edgecolor','none');
-set(fig,'Name','RAW');
-xlabel('phi [deg]');
-ylabel('theta [deg]');
+
+for i=1:121
+    for j =1:61
+        if abs(plog(i,j)) < 1e-6
+            plog(i,j) = -1e-6;
+        end
+    end
+end
+
+% Plot Surface
+% plot2d(phi_int, theta_int, -plog, [0 90], 'RAW');
+
+% ------------------------------------------------------------------------
+% Obtain the Vectors
+[x_int,y_int,z_int] = sph2cart(phi_int*pi/180,theta_int*pi/180, plog); %[x,y,z] = sph2cart(azimuth,elevation,r)
 
 % ------------------------------------------------------------------------
 % Obtain Measurement Data
-[azi_int,elev_int,r] = cart2sph(x_int,y_int,z_int); % THis function is not 100%
-
-% Refine Data
-for i = 1:121
-    for j = 1:61
-        azi(i,j) = azi_int(2,j)*180/pi;
-        elev(i,j) = elev_int(i,61)*180/pi;
-    end
-end
-
-
-% Set Plot
-di = 121;  % elev (121) -90 to 90  180 deg axis -> -z_axis -x direction
-dj = 61;   %  azi (61) -180 to 180 360 deg axis -> -x_axis x-y plane +z direction
-
-% Mapping
-for i = 1:1:61   
-    for j=1:1:61
-        azi(i,j) = azi(i,j);
-        elev(i,j) = pi/2 - elev(i,j);
-    end
-end
+[azi_int,elev_int,r_int] = bst_cart2sph(x_int,y_int,z_int); % THis function is not 100%
 
 
 for i = 1:1:121      % ele
     for j=1:1:61   % azi
-        fprintf('[%3d,%3d] azi: %8.2f ele: %8.2f\n',...
-            i,j,azi(i,j),elev(i,j));
         
         % Rotate Vector
-        vector_test(:,i,j)   = [x_int(i,j);y_int(i,j);z_int(i,j)];
-        vector_sc_int(:,i,j) = dcm(1,pi/2)*dcm(3,-pi/2)*vector_test(:,i,j);       % BST
-        vector_sc(:,i,j)     = dcm(3,-pi/2)*dcm(1,-pi/2)*vector_sc_int(:,i,j);    % STM
+        vector_test(:,i,j) = [x_int(i,j);y_int(i,j);z_int(i,j)];
+        vector_sc(:,i,j)   = dcm(1,pi/2)*dcm(3,-pi/2)*vector_test(:,i,j);       % BST
+        vector_stm(:,i,j)     = dcm(3,-pi/2)*dcm(1,-pi/2)*vector_sc(:,i,j);    % STM
         
         % Test Frame
         x_test(i,j) = vector_test(1,i,j);
@@ -100,138 +81,55 @@ for i = 1:1:121      % ele
         y_sc(i,j) = vector_sc(2,i,j);
         z_sc(i,j) = vector_sc(3,i,j);
         
-        mag_sc(i,j)  = sqrt( x_sc(i,j)^2 + y_sc(i,j)^2 + z_sc(i,j)^2);    
+        mag_sc(i,j)  = sqrt( x_sc(i,j)^2 + y_sc(i,j)^2 + z_sc(i,j)^2);  
+        
+         % STM Frame
+        x_stm(i,j) = vector_stm(1,i,j);
+        y_stm(i,j) = vector_stm(2,i,j);
+        z_stm(i,j) = vector_stm(3,i,j);
+        
+        mag_stm(i,j)  = sqrt( x_stm(i,j)^2 + y_stm(i,j)^2 + z_stm(i,j)^2);  
     end   
     fprintf('\n');
 end
 
+% ------------------------------------------------------------------------
+% Spacecraft 
+[azi_sc,elev_sc,r_sc] = bst_cart2sph(x_sc,y_sc,z_sc);
+
+
+
+% STM 
+[azi_stm,elev_stm,r_stm] = bst_cart2sph(x_stm,y_stm,z_stm);
+
+
+if (0)
 % -------------------------------------------------------------------------
 % Plot
-type ='test';
+figure
+contour(azi_int*180/pi, elev_int*180/pi, r_int,70, 'Fill', 'on');
 
-switch type
-  case'test'
-    xx  = x_test;
-    yy  = y_test;
-    zz  = z_test;   
-    mag = mag_test;
-  case 'body'
-    xx  = x_sc;
-    yy  = y_sc;
-    zz  = z_sc;
-    mag = mag_sc;
-  otherwise
+figure
+scatterplot(azi_int*180/pi,elev_int*180/pi,r_sc);
+
+plot2d(azi_int*180/pi, elev_int*180/pi, r_int, [0 -90], 'TEST');
+plot3d(x_test,y_test,z_test,mag_test, 'test'); 
+else
+
+% Spacecraft
+figure
+scatterplot(azi_sc*180/pi,elev_sc*180/pi,r_sc);
+
+figure
+contour(azi_sc*180/pi, elev_sc*180/pi, r_sc,70, 'Fill', 'on');
+
+plot2d(azi_sc*180/pi, elev_sc*180/pi, r_sc, [0 -90], 'SPACECRAFT');
+plot3d(x_sc,y_sc,z_sc,r_sc, 'spacecraft'); 
 end
 
-
-% cal pattern combined / total
-pc1 = abs(pattern.value.cal(:,:,freq,1));
-pc2 = abs(pattern.value.cal(:,:,freq,2));
-pc  = sqrt((pc1 .* pc1) + (pc2 .* pc2));
-
-% Gmax 
-Glin = (max(max(pc(:,:))))^2;
-Glog = max(max(20*log10(pc(:,:))));
-
-% Efficiency
-effeciency = Glin / Dlin;
-
-% Magnitude Calibration
-zcal = max(20*log10(pc(:,:)), Glog-dynamic); 
-
-% Plot
-screensize = get(0,'ScreenSize');
-
-fig = figure;
-set(fig,'Position',[screensize(3)*0.5 screensize(4)*0.25  screensize(3)*0.5 screensize(4)*0.5]);
-set(fig,'Name',upper(type));
-
-h3 = surf(xx,yy,zz,-plog); hold on;
-
-axis equal; axis off;
-cameratoolbar('SetMode','orbit')   
-cameratoolbar('Show')
-cameratoolbar('SetCoordSys','none')
-set(gca,'CameraViewAngle',8); % Set Zoom of Graph
-set(get(colorbar,'title'),'String', 'gain [dBi]', 'FontSize', 18, 'fontname', 'arial'); 
-% caxis manual; 
-caxis([-20 30])
-set(h3,'edgecolor','none'); 
-% shading interp;
-
-
-
-% Koordinatensystem
-length = 50;
-width  = 1;
-xachse = plot3([0 length],[0 0],[0 0],'LineWidth',width, 'color', 'black');
-xtext  = text([length],[0],[0],'x','fontname','arial','FontSize',18);
-
-yachse = plot3([0 0],[0 length],[0 0],'LineWidth',width, 'color', 'black');
-ytext  = text([0],[length],[0],'y','fontname','arial','FontSize',18);
-
-zachse = plot3([0 0],[0 0],[0 length],'LineWidth',width, 'color', 'black');
-ztext  = text([0],[0],[length],'z','fontname','arial','FontSize',18);  
-
-% view(120,20);
-
-
-usedfreq = pattern.dim(3).value(freq);
-% title([aut.name,', ' num2str(usedfreq),' MHz',', Gmax = ',num2str(roundn(Glog,-1)),' dBi'], 'fontname', 'arial', 'fontsize', 20);
-%title(['5-miniSMP, ' num2str(usedfreq),' GHz',', Gmax = ',num2str(roundn(Gmax,-1)),' dBi'], 'fontname', 'arial', 'fontsize', 22);
-%text('Dmax = ',Dlog);
-
-% rotate3d on; 
-
-% [X,Y] = meshgrid(0.5:0.5:5,1:20);
-% % X - 20x10
-% % Y - 20x10
-% % Z - 20x10
-% Z = sin(X) + cos(Y);
 % figure
-% surf(X,Y,Z)
-return
-%% ---------------------------------------------------------------------------
-% 2D Plot
-fig = figure;
-% x_data = pattern.measurement.dim(1).value;
-% y_data = pattern.measurement.dim(2).value;
-% z_data = 20*log10(pc(:,:)');
+% scatterplot(azi_stm*180/pi,elev_stm*180/pi,r_stm);
+% 
+% plot2d(azi_stm*180/pi, elev_stm*180/pi, r_stm, [0 -90], 'STM');
+% plot3d(x_stm,y_stm,z_stm,r_stm, 'stm'); 
 
-% Set plot
-x_data = azi*180/pi;  % 
-y_data = elev*180/pi;    % 
-z_data = mag;
-
-% set(fig,'Position',[screensize(3)*0.5 screensize(4)*0.5  screensize(3)*0.5 screensize(4)*0.5]);
-h4 = surf(x_data,y_data, z_data, 'EdgeColor','none');
-xlabel('Elevation: theta 0 to 180');
-ylabel('Azimuth:   phi -180 to 180');
-zlabel('Magnitude');
-view([0 -90])
-
-
-return
-
-
-% set(gca, 'XLim', [x_data y_data]);
-set(gca, 'xtick', y_data);
-
-% set(get(colorbar,'title'),'String', 'gain [dBi]', 'FontSize', 18, 'fontname', 'arial'); 
-
-%ax1.XLim = [pattern.measurement.dim(1).value(end) pattern.measurement.dim(1).value(1)];
-% set(gca, 'YLim', [x_data y_data]);
-x_data = x_data';
-set(gca, 'ytick', x_data);
-%ax1.YLim = [pattern.measurement.dim(2).value(1) pattern.measurement.dim(2).value(end)];
-
-
-set(ax1, 'CLim', [Glog-30 Glog]);
-% %ax1.CLim = [Glog-30 Glog];
- set(ax1, 'XLabel', '\theta (deg)');
-% %ax1.XLabel.String  = '\theta (deg)';
- set(ax1, 'YLabel', '\phi (deg)');
-% %ax1.YLabel.String  = '\phi (deg)';
- set(ax1, 'Title', 'BST Patchantenna, 2230MHz, Gnax= 6dBi, Combined Gain (\theta,\phi) in dBi');
-%ax1.Title.String   = ['Kombinierter Gewinn (\theta,\phi) in dBi'];
-%print('C:\Users\wiedfeld\Desktop\2D_pattern.pdf','-S720,360');
